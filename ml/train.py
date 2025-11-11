925c561dbf6f81956b79579b0106b598005ee70b
= <sajjadalizadeh275800@gmail.com>
2025-11-11 15:44:12 +0330

    log and register model

--- ml/train.py
+++ ml/train.py
@@ -2,8 +2,11 @@ import os
 import time
 import mlflow
 import pandas as pd
+from mlflow.models.signature import infer_signature
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import mlflow.sklearn
+
 from sklearn.metrics import mean_squared_error
 import joblib
 
@@ -21,7 +24,8 @@ y = df["price"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-with mlflow.start_run():
+
+with mlflow.start_run() as run:
     model = LinearRegression()
     model.fit(X_train, y_train)
     preds = model.predict(X_test)
@@ -33,6 +37,14 @@ with mlflow.start_run():
     os.makedirs("ml", exist_ok=True)
     joblib.dump(model, "ml/model.pkl")
 
-    mlflow.sklearn.log_model(model, "model")
+    signature = infer_signature(X_train, model.predict(X_train))
+    input_example = X_train.iloc[:5]
 
-print("✅ Model trained and logged to MLflow.")
+    # Log the model
+    mlflow.sklearn.log_model(model, "model", signature=signature, input_example=input_example)
+
+    # Register it in the Model Registry
+    model_uri = f"runs:/{run.info.run_id}/model"
+    registered_model = mlflow.register_model(model_uri, "HousePriceModel")
+
+print(f"✅ Model registered as '{registered_model.name}' version {registered_model.version}")
